4edf50d6bf20df5c35de50978c0ba073650efca6
ozeonmd <ozeon@warain.com>
2018-12-01 23:23:44 -0600

    facilitates token buying

--- contracts/PanTokenSale.sol
+++ contracts/PanTokenSale.sol
@@ -6,11 +6,22 @@ contract PanTokenSale {
     address admin;
     PanToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokenSold;
 
     function PanTokenSale(PanToken _tokenContract, uint256 _tokenPrice) public {
         admin = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
-        // Token price
     }
+
+    // Buy PanTokenSale
+    function buyTokens(uint256 _numberOfTokens) public payable {
+      // Require that value is equal to token tokenPrice
+      // Require enough contract has enough tokenSold
+      // Require that a transfer is successful
+      // Keep track of tokenSold
+      tokenSold += _numberOfTokens;
+      // Trigger Sell event
+    }
+    // end sale
 }
--- test/PanTokenSale.js
+++ test/PanTokenSale.js
@@ -2,21 +2,40 @@ var PanTokenSale = artifacts.require("./PanTokenSale.sol");
 
 
 contract('PanTokenSale', function(accounts){
-  var PanTokenSaleInstance;
+  var tokenSaleInstance;
+  var buyer = accounts[1];
   var tokenPrice = 1000000000000000; //in wei
+  var numberOfTokens;
 
   it('initializes the contracts with the correct values', function(){
     return PanTokenSale.deployed().then(function(instance){
-      PanTokenSaleInstance = instance;
-      return PanTokenSaleInstance.address
+      tokenSaleInstance = instance;
+      return tokenSaleInstance.address
     }).then(function(address){
       assert.notEqual(address, 0x0, ' has contract address');
-      return PanTokenSaleInstance.tokenContract();
+      return tokenSaleInstance.tokenContract();
     }).then(function(address){
       assert.notEqual(address, 0x0, 'has a token contract address');
-      return PanTokenSaleInstance.tokenPrice();
+      return tokenSaleInstance.tokenPrice();
     }).then(function(price){
       assert.equal(price, tokenPrice, 'token price is correct')
     });
   });
+  it('facilitates token buying', function(){
+      return PanTokenSale.deployed().then(function(instance){
+        tokenSaleInstance = instance;
+        numberOfTokens = 10;
+        return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice})
+      }).then(function(receipt){
+        return tokenSaleInstance.tokenSold();
+      }).then(function(amount) {
+        assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold');
+      });
+  });
+
+
+
+
+
+
 });
